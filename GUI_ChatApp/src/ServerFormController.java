9a35446cf52503439a6d2f6f1b5d1b591de20b2e
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-03 09:17:59 +0530

    Content Updated

--- GUI_ChatApp/src/ServerFormController.java
+++ GUI_ChatApp/src/ServerFormController.java
@@ -1,11 +1,38 @@
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerFormController {
     public TextArea textArea;
     public TextField textField;
+
+    final int PORT = 5000;
+    ServerSocket serverSocket;
+    Socket accept;
+    DataInputStream dataInputStream;
+    DataOutputStream dataOutputStream;
+
+    public void initialize() {
+        new Thread(() -> {
+            try {
+                serverSocket = new ServerSocket(PORT);
+                System.out.println("Server Started..");
+                accept = serverSocket.accept();
+                System.out.println("Client Connected..");
+
+                dataOutputStream = new DataOutputStream(accept.getOutputStream());
+                dataInputStream = new DataInputStream(accept.getInputStream());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
     public void sendOnAction(ActionEvent actionEvent) {
     }
