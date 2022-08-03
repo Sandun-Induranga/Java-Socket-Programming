a7be6d66edfb20042e2315fd1de1999e0c254bbe
Sandun-Induranga <sandutharu40@gmail.com>
2022-08-03 08:55:39 +0530

    Content Updated

--- GUI_ChatApp/src/ClientFormController.java
+++ GUI_ChatApp/src/ClientFormController.java
@@ -1,2 +1,11 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ClientFormController {
+    public TextArea textArea;
+    public TextField textMessage;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
 }
--- GUI_ChatApp/src/ServerFormController.java
+++ GUI_ChatApp/src/ServerFormController.java
@@ -1,2 +1,12 @@
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class ServerFormController {
+    public TextArea textArea;
+    public TextField textField;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
 }
